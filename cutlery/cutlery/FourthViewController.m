347e9f08ffb006c1e8e1489d246dea5cc7be39e4
Russell Nibbelink <russell.n@berkeley.edu>
2014-10-11 14:39:37 -0700

    4 tabs

---
+++ cutlery/cutlery/FourthViewController.m
@@ -0,0 +1,27 @@
+//
+//  FourthViewController.m
+//  cutlery
+//
+//  Created by Russell Nibbelink on 10/11/14.
+//  Copyright (c) 2014 Appstract. All rights reserved.
+//
+
+#import "FourthViewController.h"
+
+@interface FourthViewController ()
+
+@end
+
+@implementation FourthViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ cutlery/cutlery/ThirdViewController.m
@@ -0,0 +1,27 @@
+//
+//  ThirdViewController.m
+//  cutlery
+//
+//  Created by Russell Nibbelink on 10/11/14.
+//  Copyright (c) 2014 Appstract. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
